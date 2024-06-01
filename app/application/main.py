c41c357843432e13bf214b2c696289960f933ff1
Dmitry Patrushev <impda@mail.ru>
2024-06-01 11:36:50 +0300

    Hardcoded constant moved to config

--- app/application/main.py
+++ app/application/main.py
@@ -9,7 +9,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers.base import BaseScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from logger.base import create_logger
+from logic.init import init_container
 from logic.send_random_message import send_random_image_and_text
+from settings.config import Config
 
 
 def error_listener(event: JobExecutionEvent) -> None:
@@ -29,12 +31,15 @@ if __name__ == '__main__':
     logger = create_logger('app')
     logging.getLogger('apscheduler.executors.default').disabled = True
 
+    container = init_container()
+    config: Config = container.resolve(Config)
+
     loop = asyncio.new_event_loop()
 
     scheduler = AsyncIOScheduler(event_loop=loop)
     scheduler.add_listener(error_listener, EVENT_JOB_ERROR)
 
-    trigger = IntervalTrigger(minutes=60)
+    trigger = IntervalTrigger(minutes=config.posting_interval)
     job = scheduler.add_job(send_random_image_and_text, trigger)
 
     loop.add_signal_handler(signal.SIGTERM, functools.partial(graceful_stop, scheduler))
--- app/settings/config.py
+++ app/settings/config.py
@@ -4,3 +4,4 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     ms_teams_webhook: str = Field(alias='webhook')
+    posting_interval: int = 60
