b9b9cdf0b0d97ef26c0c0c663c04a1145baf9eb4
Dmitry Patrushev <impda@mail.ru>
2024-06-17 10:15:47 +0300

    Added logic to skip configured dates via short-circuiting to the end

--- app/application/airflow/dags/msi_dag.py
+++ app/application/airflow/dags/msi_dag.py
@@ -1,12 +1,19 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 from logic.send_random_message import (
     get_random_image_url,
     get_random_quote,
     send_image_and_quote_to_teams,
 )
+
+
+def do_run_today(ds: str) -> bool:
+    mm_dd = ds.split('-', 1)[1]
+    return mm_dd not in {'11-06', '13-06', '15-06', '25-06'}
+
 
 with DAG(
     dag_id='msi_project_dag',
@@ -16,6 +23,12 @@ with DAG(
     catchup=False,
 ) as msi_project_dag:
     start = EmptyOperator(task_id='start')
+
+    skip_on_holidays = ShortCircuitOperator(
+        task_id='skip_on_holidays',
+        python_callable=do_run_today,
+        ignore_downstream_trigger_rules=False,
+    )
 
     get_image_url = PythonOperator(
         task_id='get_random_image_url',
@@ -33,6 +46,6 @@ with DAG(
         op_args=[get_quote.output, get_image_url.output],
     )
 
-    end = EmptyOperator(task_id='end')
+    end = EmptyOperator(task_id='end', trigger_rule=TriggerRule.NONE_FAILED)
 
-    start >> [get_image_url, get_quote] >> send_message >> end
+    start >> skip_on_holidays >> [get_image_url, get_quote] >> send_message >> end
